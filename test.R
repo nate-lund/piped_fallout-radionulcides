8611c1cf078f1a44366fa0be991d1408aba15966
Nate Lund <nathanlund26@gmail.com>
2026-06-10 14:07:29 -0500

    aa

---
+++ test.R
@@ -0,0 +1,30 @@
+#================================ Setup ================================
+
+# libraries needed
+libs <- c("")
+
+# install missing libraries
+installed_libs <- libs %in% rownames(installed.packages())
+if (any(installed_libs == F)) {
+  install.packages(libs[!installed_libs])
+}
+
+# load libraries
+lapply(libs, library, character.only = T)
+
+# enter the file path for the highest level folder you're working in 
+data_folder <- "C:/Users/natha/Box/"
+
+# when a file is needed, call the hert() function
+# for example; data_frame = read.csv(hert("more_data/measurements_data.csv"))
+hert <- function(file) {
+  file_path = paste(data_folder, file, sep = "")
+  return(file_path)
+}
+
+#================================ X ================================
+
+
+
+
+
