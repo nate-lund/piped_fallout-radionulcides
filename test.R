2b7d94798b06aeb2379ac071272d504d90ccece5
Nate Lund <nathanlund26@gmail.com>
2026-06-10 16:03:31 -0500

    g

--- test.R
+++ test.R
@@ -1,7 +1,5 @@
 #================================ Setup ================================
 
-#================================ Setup ================================
-
 # Clear environment
 rm(list = ls(all.names = TRUE))
 
@@ -20,14 +18,53 @@ lapply(libs, library, character.only = T)
 #================================ X ================================
 
 path = "G:/Shared drives/P05-mitppc-jumpingwormerosion/Project-Data/Fallout-Radionucldes/reference-bulk-density.xlsx"
+ref_bd = read_excel(path)
 
-data = read_excel(path)
-
-datatable(processed)
-
-processed = data %>% 
+ref_bd = ref_bd %>% 
   # Remove root ball sample
-  filter(!(site == "ARB" & replicate == "B" & depth == "0-5")) %>% 
-  group_by(site, depth) %>% 
+  filter(!(site == "ARB" & replicate == "B" & bottom_depth == 5)) %>% 
+  group_by(site, bottom_depth) %>% 
   summarise(mean = mean(bd)) %>% 
   ungroup()
+
+
+path2 = "G:/Shared drives/P05-mitppc-jumpingwormerosion/Project-Data/Fallout-Radionucldes/sample_inventory.xlsx"
+inventory = read_excel(path2)
+datatable(inventory2)
+
+inventory2 = inventory %>%
+  mutate(
+    # Compute dry mass
+    dry_mass = tin_plus_soil - tin,
+    
+    # Assign volumes based on the number of sampling points and volume of sample
+    volume = case_when(
+      forest %in% c("ASH", "WD", "MAG") ~ "510",
+      forest %in% c("LRJ", "LRW", "LRE") ~ "510",
+      forest == "ARB_ref" ~ "425", 
+      forest == "LR_ref" ~ "453.33",
+      TRUE ~ ""
+    ),
+    
+    volume = as.numeric(volume),
+    
+    bd = dry_mass / volume
+  )
+
+# Look at only reference samples
+probe_ref = inventory2 %>% 
+  select(site, forest, bottom_depth, dry_mass, volume, bd) %>% 
+  filter(forest %in% c("ARB_ref", "LR_ref"))
+
+
+# Join refernece samples collect with ring method and probe method
+
+
+joined = left_join(probe_ref, ref_bd, by = c("site", "bottom_depth")) %>% 
+  select(site, bottom_depth, bd, mean)
+
+ggplot(data = joined, mapping = aes(x = bd, y = mean, color = bottom_depth)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed")
+
+
